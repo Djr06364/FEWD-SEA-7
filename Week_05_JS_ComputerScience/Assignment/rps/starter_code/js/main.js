d9fa75ce6fe2cd910135e3418a9b8dbfc2e18ee2
Dan Reina <danieljreina@gmail.com>
2016-01-27 19:47:54 -0800

    RPS
    all of it

--- Week_05_JS_ComputerScience/Assignment/rps/starter_code/js/main.js
+++ Week_05_JS_ComputerScience/Assignment/rps/starter_code/js/main.js
@@ -6,13 +6,77 @@
 // You can mix variables with strings (text wrapped in quotes)
 
 // add your functions here
+$(document).ready(function(){
+
+var i = 1
+var u = 1
+
+	$("#rock").click(function(){
+		
+		if (getComputerPlay() === "rock"){
+		$("#winner").html("TIE GAME");
+		$("#body").css("background-color","white");
+		}
+
+		else if (getComputerPlay() === "scissors"){
+			$("#winner").html("HUMAN WINS");
+			$("#humanScore").html(i++);
+			$("#body").css("background-color","green");
+		}
+
+		else {	$("#computerScore").html(u++);
+		$("#winner").html("COMPUTER WINS");
+		$("#body").css("background-color","red");
+		}
+
+	});
+	$("#scissors").click(function(){
+		
+
+	if (getComputerPlay() === "scissors"){
+		$("#winner").html("TIE GAME");
+		}
+
+		else if (getComputerPlay() === "rock"){
+			$("#winner").html("HUMAN WINS");
+			$("#humanScore").html(i++);
+		}
+
+		else {	$("#computerScore").html(u++);
+		$("#winner").html("COMPUTER WINS");
+		}
+
+
+
+	});
+	$("#paper").click(function(){
+		
+	if (getComputerPlay() === "paper"){
+		$("#winner").html("TIE GAME");
+		}
+
+		else if (getComputerPlay() === "rock"){
+			$("#winner").html("HUMAN WINS");
+			$("#humanScore").html(i++);
+		}
+
+		else {	$("#computerScore").html(u++);
+		$("#winner").html("COMPUTER WINS");
+		}
+
+
+	});
 
 function getComputerPlay() {
-  // Create an array called plays below this line. It should
-  // have the options for the RPS game.
+
+  var plays = ["rock", "paper", "scissors"];
   var play = plays[Math.floor(Math.random() * plays.length)];
   return play;
-}
 
-// add your doc ready statement right
-// this is where you'll bind evnet (hint: click)
+}
+console.log(getComputerPlay());
+
+
+});
+
+
